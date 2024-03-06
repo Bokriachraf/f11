e13890362dfd143aad94e6a72a8a8cccd4fc202b
achrafbokri <achrafbokri@gmail.com>
2024-03-06 15:36:27 +0100

    V1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+   return (
+    <div className="rid-container">
+    <header className="row">
+     <div>
+        <a className="brand" href="index.html">MernExemple</a>
+     </div>
+     <div>
+        <a href="cart.html">cart</a>
+        <a href="signin.html">signin</a>
+     </div>
+    </header>
+    <main>
+        <div>
+     <div className="row center">
+       <div className="card">
+        <a href="product.html">
+            <img className="medium" src="./image/p2.jfif" alt="product"/>
         </a>
-      </header>
+        <div className="card-body">
+         <a href="product.html">
+            <h2>Adidas shirt</h2>
+         </a>
+         <div className="rating">
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            </div>
+            <div className="price">133Dt</div>
+        </div>
+       </div>
+       <div className="card">
+        <a href="product.html">
+            <img className="medium" src="./image/p3.jfif" alt="product"/>
+        </a>
+        <div className="card-body">
+         <a href="product.html">
+            <h2>Nike shirt</h2>
+         </a>
+         <div className="rating">
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            </div>
+            <div className="price">113Dt</div>
+        </div>
+       </div>
+       <div className="card">
+        <a href="product.html">
+            <img className="medium" src="./image/p4.jfif" alt="product"/>
+        </a>
+        <div className="card-body">
+         <a href="product.html">
+            <h2>Balance Ti_shirt</h2>
+         </a>
+         <div className="rating">
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            </div>
+            <div className="price">139Dt</div>
+        </div>
+       </div>
+       <div className="card">
+        <a href="product.html">
+            <img className="medium" src="./image/p5.jfif" alt="product"/>
+        </a>
+        <div className="card-body">
+         <a href="product.html">
+            <h2>Puma shirt</h2>
+         </a>
+         <div className="rating">
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            </div>
+            <div className="price">154Dt</div>
+        </div>
+       </div>
+       <div className="card">
+        <a href="product.html">
+            <img className="medium" src="./image/p1.jfif" alt="product"/>
+        </a>
+        <div className="card-body">
+         <a href="product.html">
+            <h2>Adidas shirt</h2>
+         </a>
+         <div className="rating">
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            <span><i className="fa fa-star"></i></span>
+            </div>
+            <div className="price">133Dt</div>
+        </div>
+       </div>
     </div>
+     </div>
+    </main>
+    <footer className="row center">
+      Contact
+    </footer>
+</div>
   );
 }
 
